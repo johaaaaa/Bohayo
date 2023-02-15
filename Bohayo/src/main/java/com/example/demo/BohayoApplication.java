cf5830724a2ab68d24967b6bb0521bbc29db9cef
bbk11 <bbk11@LAPTOP-4FFJDIPM>
2023-02-15 16:58:23 +0900

    0215

---
+++ Bohayo/src/main/java/com/example/demo/BohayoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BohayoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BohayoApplication.class, args);
+	}
+
+}
